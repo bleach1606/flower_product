72f0cfe6bd447c7e6274728a08c7225485599d7c
bleach1606 <bl.bleach1606@gmail.com>
2020-05-02 13:58:44 +0700

    con fix bug API :)))

--- src/main/java/ptit/edu/btl/controller/PublicController.java
+++ src/main/java/ptit/edu/btl/controller/PublicController.java
@@ -3,7 +3,10 @@ package ptit.edu.btl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ptit.edu.btl.entity.Token;
 import ptit.edu.btl.entity.Users;
 import ptit.edu.btl.repository.TokenRepository;
@@ -11,6 +14,11 @@ import ptit.edu.btl.repository.UsersRepository;
 import ptit.edu.btl.util.ResponseJson;
 import ptit.edu.btl.controller.BaseController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 import java.util.UUID;
 
@@ -50,5 +58,22 @@ public class PublicController extends BaseController {
             responseJson.setMessage(ex.getMessage());
             return createErrorResponse(ex.getMessage(), HttpStatus.resolve(403));
         }
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = Paths.get("" + fileName);
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/download/")
+                .path(fileName)
+                .toUriString();
+        return ResponseEntity.ok(fileDownloadUri);
     }
 }
